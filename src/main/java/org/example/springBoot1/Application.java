aacebbf4e427aa8f2d269ea642414f9bf3ad082e
주재현 <jake0104@naver.com>
2021-12-15 20:40:25 +0900

    p.150.12

--- src/main/java/org/example/springBoot1/Application.java
+++ src/main/java/org/example/springBoot1/Application.java
@@ -4,7 +4,7 @@ package org.example.springBoot1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-//fd
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class Application {
